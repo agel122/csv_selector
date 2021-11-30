74bc223e6fd0ef666149bcd093517d773d34f891
agel122 <agel122@yandex.ru>
2021-11-30 19:02:18 +0300

    2021_11_30_views_added

--- csvreader/models.py
+++ csvreader/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class CSVdata(models.Model):
-    filename = models.CharField(max_length=50)
+    filename = models.CharField(max_length=50, unique=True)
     dataresult = models.DecimalField(max_digits=20, decimal_places=16)
 
     def __str__(self):
--- csvreader/views.py
+++ csvreader/views.py
@@ -1,11 +1,30 @@
+import csv
+import os
+
 from rest_framework import generics
+from rest_framework import viewsets
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import CSVdata
 from .serializers import ResultSerializer
 
+from .readfunc import csvsumm, set_file_dir
+
 
 class StartCalc(APIView):
-    pass
+    serializer_class = ResultSerializer
+
+    def post(self, request):
+        file_data = self.request.data
+        file_required = set_file_dir(file_data["filename"])
+        file_dataresult = csvsumm(file_required, 9)
+
+        serializer = ResultSerializer(data={'filename': file_data["filename"], 'dataresult': file_dataresult})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class CountInWork(APIView):
@@ -13,7 +32,9 @@ class CountInWork(APIView):
 
 
 class GetResult(APIView):
-    pass
 
-
-
+    def get(self, request, *args, **kwargs):
+        filename = '.'.join([self.request.query_params.get('filename'), 'csv'])
+        required_info = CSVdata.objects.get(filename=filename)
+        serializer = ResultSerializer(required_info)
+        return Response(serializer.data)
