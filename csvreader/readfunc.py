d9f043296f73d518577ace53056a31883c3ad11a
agel122 <agel122@yandex.ru>
2021-11-30 11:39:14 +0300

    2021_11_30_added_countfunc(corrected path)

--- csvreader/readfunc.py
+++ csvreader/readfunc.py
@@ -1,9 +1,11 @@
-import csv, os
-from pathlib import Path
+import csv
+import os
+
+# my_file = os.path.join('/home/aman/Yandex.Disk/IT/test_excercise/files', 'data.csv')
 
 
 def set_file_dir(file_name):
-    file_path = os.path.join(Path().absolute().parent.parent, 'files', file_name)
+    file_path = os.path.join(os.path.abspath(os.path.join(__file__,"../..")), 'files', file_name)
     return file_path
 
 
@@ -20,8 +22,9 @@ def csvsumm(file_to_count, column_to_sum):
     return summed
 
 
-my_file = set_file_dir('data.csv')
-print(csvsumm(my_file, 9))
+# my_file = set_file_dir('data.csv')
+# print(csvsumm(my_file, 9))
+
 
 
 
