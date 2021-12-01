0994275aea03a1374ee3e041179d9f317dd6c46e
agel122 <agel122@yandex.ru>
2021-12-01 23:48:02 +0300

    2021_12_01_AddResult doesn't work

--- csvreader/urls.py
+++ csvreader/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import StartCalc, CountInWork, GetResult
+from .views import StartCalc, CountInWork, GetResult, AddResult
 
 
 urlpatterns = [
-    path('start/', StartCalc.as_view()),
-    path('ongoing/', CountInWork.as_view()),
-    path('getresult/', GetResult.as_view()),
+    path('start', StartCalc.as_view()),
+    path('ongoing', CountInWork.as_view()),
+    path('getresult', GetResult.as_view()),
+    path('data_adder', AddResult.as_view()),
 ]
--- csvreader/views.py
+++ csvreader/views.py
@@ -1,6 +1,7 @@
 import csv
 import os
 
+from django.http import JsonResponse
 from rest_framework import generics
 from rest_framework import viewsets
 from rest_framework import status
@@ -20,21 +21,40 @@ class StartCalc(APIView):
         file_required = set_file_dir(file_data["filename"])
         file_dataresult = csvsumm(file_required, 9)
 
-        serializer = ResultSerializer(data={'filename': file_data["filename"], 'dataresult': file_dataresult})
+        serializer = ResultSerializer(data={
+            'filename': file_data["filename"],
+            # 'dataresult': file_dataresult,
+            'status': 'in_work'
+        })
         if serializer.is_valid():
             serializer.save()
+            # my_task.apply_async(task_id = serializer.data.id)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class CountInWork(APIView):
-    pass
+    def get(self, request):
+        inprogress_count = CSVdata.objects.filter(status='in_work').count()
+        return JsonResponse({'tasks in work': inprogress_count})
 
 
 class GetResult(APIView):
-
     def get(self, request, *args, **kwargs):
         filename = '.'.join([self.request.query_params.get('filename'), 'csv'])
         required_info = CSVdata.objects.get(filename=filename)
         serializer = ResultSerializer(required_info)
         return Response(serializer.data)
+
+
+class AddResult(APIView):
+    def post(self, request):
+        file_data = self.request.data
+        file_required = set_file_dir(file_data["filename"])
+        file_dataresult = csvsumm(file_required, 9)
+        item_to_update = CSVdata.objects.get(file_data["filename"])
+        item_to_update.dataresult = file_dataresult
+        item_to_update.save(['dataresult'])
+        serializer = ResultSerializer(item_to_update)
+        return Response(serializer.data)
+
