fe9e9b70aae0945a6c9cf79e348a6e4a48ebbaa4
agel122 <agel122@yandex.ru>
2021-12-03 20:29:51 +0300

    2021_12_03_task have been added. works. no tests yet.

--- csvreader/views.py
+++ csvreader/views.py
@@ -10,6 +10,8 @@ from rest_framework.views import APIView
 from .models import CSVdata
 from .serializers import ResultSerializer
 
+from celery import current_app
+from .tasks import upload_data
 from .readfunc import csvsumm, set_file_dir
 
 
@@ -18,8 +20,8 @@ class StartCalc(APIView):
 
     def post(self, request):
         file_data = self.request.data
-        file_required = set_file_dir(file_data["filename"])
-        file_dataresult = csvsumm(file_required, 9)
+        # file_required = set_file_dir(file_data["filename"])
+        # file_dataresult = csvsumm(file_required, 9)
 
         serializer = ResultSerializer(data={
             'filename': file_data["filename"],
@@ -29,6 +31,7 @@ class StartCalc(APIView):
         if serializer.is_valid():
             serializer.save()
             # my_task.apply_async(task_id = serializer.data.id)
+            upload_data.delay(file_data["filename"])
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
