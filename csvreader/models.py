41a67d9e107b4ccda7981753503de9e40bb64e6f
agel122 <agel122@yandex.ru>
2021-12-03 23:12:18 +0300

    2021_12_03_corrected README.md

--- csvreader/models.py
+++ csvreader/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class CSVdata(models.Model):
     filename = models.CharField(max_length=50, unique=True)
     dataresult = models.DecimalField(max_digits=20, decimal_places=16, blank=True, null=True)
-    status = models.CharField(max_length=20)
+    status = models.CharField(max_length=20, help_text='there are two statuses: "in_work" or "done"')
 
     def __str__(self):
         return str(self.filename)
