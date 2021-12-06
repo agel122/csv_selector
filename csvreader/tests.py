f38cb95049fb38f21b64ff2f2e1a53dd84f669d4
agel122 <agel122@yandex.ru>
2021-12-06 23:18:52 +0300

    2021_12_06_tests are added

--- csvreader/tests.py
+++ csvreader/tests.py
@@ -33,10 +33,13 @@ class AddTestCase(APITestCase):
     def testFinalResult(self):
         data = {'filename': 'data.csv'}
         self.client.post('/api/start', data)
-        response = self.client.get('/api/getresult?filename=data')
-        self.assertEqual(response.data["filename"], "data.csv")
-        self.assertEqual(response.data["status"], "done")
-        self.assertEqual(response.data["dataresult"], '89.2416354295115000')
+        response1 = self.client.get('/api/getresult?filename=data')
+        response2 = self.client.get('/api/ongoing')
+        self.assertEqual(response1.data["filename"], "data.csv")
+        self.assertEqual(response1.data["status"], "done")
+        self.assertEqual(response1.data["dataresult"], '89.2416354295115000')
+        self.assertEqual(response2.json(), {"tasks in work": 0})
+
 
 
 
